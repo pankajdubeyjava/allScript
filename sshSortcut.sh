80e93ecb22c293b5454228261f8620129b21c659
Pankaj dubey <dubeypd09@gmail.com>
2023-02-26 21:15:53 +0530

    Create sshSortcut.sh
    Have to add on this file path .ssh/config

---
+++ sshSortcut.sh
@@ -0,0 +1,23 @@
+Host adt2 
+    HostName ipdenahai      
+    User usenamedenahai
+
+Host tel
+    HostName ipdenahai      
+    User pankajD
+
+Host adt
+    HostName ipdenahai      
+    User pankajD
+
+Host jadt 
+    HostName  IPdenahai      
+    User pankajD
+
+
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
+~                                                                               
